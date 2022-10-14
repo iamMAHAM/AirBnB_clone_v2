bd5f1b83b1932c0bec8787933b1122806ad10e9d
iamMAHAM <abdulkabore@gmail.com>
2022-10-14 12:38:26 +0000

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    "Return a message on client"
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    'displays HBNB'
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    'displays C followed by params text'
+    return 'C {0}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    'displays Python followed by the value of the text'
+    return 'Python {0}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n: int):
+    'display “n is a number” only if n is an integer'
+    return '{0} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numberTemplate(n: int):
+    'display a HTML page only if n is an integer:'
+
+    template = '''
+    <!DOCTYPE html>
+        <HTML lang="en">
+            <HEAD>
+                <TITLE>HBNB</TITLE>
+            </HEAD>
+            <BODY>
+                <H1>Number: {0}</H1>
+            </BODY>
+        </HTML>
+    '''.format(n)
+    return template
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
